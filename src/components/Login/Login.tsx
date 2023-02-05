752bbdfb5962866dd6b728809f106357fb7c6844
Petro <thisnameisundefined@gmail.com>
2023-02-05 22:57:32 +0200

    Finished login page

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -1,9 +1,24 @@
-import React from 'react';
+import React, {useState} from 'react';
+import {Button, TextField} from "@mui/material";
+import './login.css'
 
 function Login() {
+    const [value, setValue]=useState('')
+    const onSubmit = ()=>{
+        if(!value){
+            alert('Будь ласка, введіть пароль!')
+        }else {
+            alert(value) //make post
+            setValue('')
+        }
+    }
+
   return (
-    <div>
-      here will be login form
+    <div className={'login-page'}>
+         <TextField label="Пароль" autoComplete="off" variant="outlined" size='small' onChange={(e)=> {setValue(e.target.value)}} value={value}/>
+         <span style={{marginLeft: '10px', lineHeight: '35px'}} >
+             <Button variant="contained" onClick={onSubmit}>Увійти</Button>
+         </span>
     </div>
   );
 }
